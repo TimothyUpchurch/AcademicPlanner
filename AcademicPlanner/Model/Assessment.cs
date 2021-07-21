a8c837dcde653a6fc730bf16eafd61571be338ea
Timothy <timupc94@gmail.com>
2021-07-21 08:39:57 -0500

    Added Assessment Model and Service.

---
+++ AcademicPlanner/Model/Assessment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace AcademicPlanner.Model
+{
+    public class Assessment
+    {
+        [PrimaryKey, AutoIncrement]
+        public int AssessmentID { get; set; }
+        [Indexed]
+        public int CourseID { get; set; }
+        public string AssessmentType { get; set; }
+        public string AssessmentName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ AcademicPlanner/Services/AssessmentService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using AcademicPlanner.Model;
+using SQLite;
+using Xamarin.Essentials;
+
+namespace AcademicPlanner.Services
+{
+    public static class AssessmentService
+    {
+        static SQLiteAsyncConnection db;
+
+        static async Task Init()
+        {
+            if (db != null) return;
+            var databasePath = Path.Combine(FileSystem.AppDataDirectory, "AcademicPlanner.db");
+            db = new SQLiteAsyncConnection(databasePath);
+            await db.CreateTableAsync<Assessment>();
+        }
+
+        public static async Task AddAssessment(Assessment assessment)
+        {
+            await Init();
+            await db.InsertAsync(assessment);
+        }
+    }
+}
